a782994236d33de6fe493189c6b43271564c662d
Denis Eliseev <medved-ed@mail.ru>
2022-07-18 22:31:44 +0800

    Examples 13 15

---
+++ Examples19_21_23/Program.cs
@@ -0,0 +1,23 @@
+﻿void Zadacha19 ()
+{
+    //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    Console.Write ("Введите число 1 :");
+    int number = Convert.ToInt32 (Console.ReadLine ());
+    int numberV = number;
+    int numberP = 0;
+    while (numberV>0)
+    {
+        numberP=numberP*10+numberV%10;
+        numberV=numberV/10;
+    }
+    if (numberP == number)
+    {
+        Console.WriteLine ("Число является полиндромом");
+    }
+    else 
+    {
+        Console.WriteLine ("Число не является полиндромом");
+    }
+
+}
+Zadacha19 ();
