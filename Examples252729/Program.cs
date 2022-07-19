12b73b4983b513d31c46cc16b0fbd4317deafe56
Eliseev Denis <medved-ed@mail.ru>
2022-07-20 00:58:49 +0800

    add 25

--- Examples252729/Program.cs
+++ Examples252729/Program.cs
@@ -3,6 +3,12 @@
     //Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
     // 3, 5 -> 243 (3⁵)
     // 2, 4 -> 16
+    Random rand = new Random();
+    int NumA = rand.Next (1, 7);
+    int NumB = rand.Next (1, 7);
+    Console.WriteLine ("Число А = " + NumA);
+    Console.WriteLine ("Число B = " + NumB);
+    Console.WriteLine ("А^B = " + Math.Pow (NumA, NumB));
     
 }
 
@@ -17,3 +23,5 @@ void Zadacha29 ()
     // Напишите программу, которая задаёт массив из 8 элементов и выводит отсортированный по модулю массив.
     
 }
+
+Zadacha25 ();
