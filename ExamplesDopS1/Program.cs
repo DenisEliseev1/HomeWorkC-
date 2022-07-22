b89d381ae75bccca639ce890e711bc03d8065216
Eliseev Denis <medved-ed@mail.ru>
2022-07-22 17:51:53 +0800

    add dops1 zad1234

--- ExamplesDopS1/Program.cs
+++ ExamplesDopS1/Program.cs
@@ -17,14 +17,40 @@ void Zad1 ()
 
 void Zad2 ()
 {
-    
+    Console.Write ("Введите A : ");
+    int numA = Convert.ToInt32 (Console.ReadLine ());
+    Console.Write ("Введите B : ");
+    int numB = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine ("А в числе В поместиться " + (numB/numA) + " раз");
 }
 
 // Задача 3. На ввод подаётся рост трёх спортсменов. Расположить их от большего к меньшему.
 
 void Zad3 ()
 {
-    
+    Console.Write ("Введите количество спортсменов :");
+    int NumS = Convert.ToInt32 (Console.ReadLine ());
+    int [] rost = new int [NumS];
+    Console.WriteLine ("Рост спортсменов :");
+    AddArray (rost);
+    PrintArray (rost);
+    int help = 0;
+    for (int i = 0; i < NumS-1; i++)
+    {
+        int max = i;
+        for (int j = i+1; j < NumS; j++)
+        {
+            if (rost [j] > rost[max])
+            {
+                
+                help = rost [j];
+                rost [j] = rost [max];
+                rost [max] = help;
+            }
+        }
+    }
+    Console.WriteLine ("Сортировка по росту :");
+    PrintArray (rost);
 }
 
 // Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
@@ -33,7 +59,37 @@ void Zad3 ()
 void Zad4 ()
 
 {
-    
+    Console.Write ("Введите количество секунд :");
+    int Sec = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine ($"Время в фармате Ч:М:С -> {Sec/3600} : {(Sec/60)%10} : {Sec%100}");
 }
 
-Zad1 ();
+void AddArray (int [] array)
+{
+    Random rand = new Random();
+    for (int i=0; i<array.Length; i++)
+    {
+        array [i] = rand.Next (180, 201);
+    }
+}
+
+void PrintArray (int [] array)
+{
+
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write ((i+1) + "   ");
+    }
+
+    Console.WriteLine ();
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write (array [i] + " ");
+    }
+    Console.WriteLine ();
+}
+
+//Zad1 ();
+//Zad2 ();
+//Zad3 ();
+Zad4 ();
